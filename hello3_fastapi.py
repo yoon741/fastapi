efbe2801a943c5ecd77d37cc27d23111d99eaf02
yoon741 <mono74140@gmail.com>
2024-08-12 17:34:48 +0900

    경로 매개변수

--- hello3_fastapi.py
+++ hello3_fastapi.py
@@ -5,13 +5,22 @@ from fastapi import FastAPI
 # python -m uvicorn hello2_fastapi:app --reload
 
 app = FastAPI()
-
 @app.get('/')
 def index():
-    return 'Hello, World, again!!'
+    return 'Hello, World!!'
+
+# 추천 하지 않는 방식
+@app.get('/sayHello')
+def sayhello(msg :str):
+    return f'Hello, {msg}!!'
+
+# RESTful API 방식
+@app.get('/sayAgain/{msg}')
+def sayagain(msg :str):
+    return f'Hello, {msg}!!'
 
 # __name__: 실행중인 모듈 이름을 의미하는 매직키워드
 # 만일, 파일을 직접 실행하면 __name__이름은 __main__으로 자동지정
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run('hello2_fastapi:app', reload=True)
+    uvicorn.run('hello3_fastapi:app', reload=True)
