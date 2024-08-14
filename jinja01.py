cc8efb21f4f5d7d171455aff8befe65ffb2e0b93
yoon741 <mono74140@gmail.com>
2024-08-14 17:04:29 +0900

    jinja2 - 템플릿엔진을 이용한 html 출력

--- jinja01.py
+++ jinja01.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, String, select
 from sqlalchemy.orm import declarative_base, Session
+from starlette.requests import Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+# Jinja2Template
+# 파이썬용 템플릿 엔진
+# 다양한 웹 프레임워크에서 템플릿 렌더링을 위해 사용
+# 템플릿(html)에 동적으로 데이터(db 조회 객체)를 삽입해서
+# 최종 결과물을 만드는 역할 담당
+# jinja.palletsprojects.com
+
 
 sqlite_url = 'sqlite:///app/cloud2024.db'
 engine = create_engine(sqlite_url, connect_args={}, echo=True)
@@ -19,6 +30,9 @@ class Zipcode(Base):
     seq = Column(String, primary_key=True)
 
 app = FastAPI()
+templates = Jinja2Templates(directory="views/templates")
+
+
 
 @app.get('/')
 def index():
@@ -28,7 +42,6 @@ def index():
 @app.get('/zipcode/{dong}')
 def zipcode(dong: str):
     result = ''
-
 
     # sessionmaker 없이 디비 객체 직접 생성
     with Session(engine) as sess:
@@ -41,7 +54,22 @@ def zipcode(dong: str):
     return f'{result}'
 
 
+
+@app.get('/zipcode2/{dong}', response_class=HTMLResponse)
+def zipcode(dong: str, req: Request):
+    # 입력한 동으로 zipcode에서 검색하고 결과를 result에 저장
+    with Session(engine) as sess:
+        stmt = select(Zipcode).where(Zipcode.dong.like(f'{dong}%'))
+        result = sess.scalars(stmt).all()
+
+    # 저장된 검색 결과를 템플릿 엔진을 이용해서 html 결과문서를 만들기 위해
+    # TemplateResponse 함수 호출
+    return templates.TemplateResponse('zipcode.html',
+                {'request': req, 'rows': result, 'sayhello': 'Hello, Jinja2!!'})
+
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run('jinja01:app', reload=True)
+
+
 
